74c91d402a61a844d985223ce19c76f828d2475b
Byron Taaka <4dbyron@gmail.com>
2018-08-01 10:46:24 +0300

    package app

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,6 @@ from app.instance.config import config_app
 import psycopg2
 import os
 
-# if config_app['testing']:
-#     db_name = os.getenv('DATABASE_TESTS')
-# else:
 db_name = os.getenv('DATABASE_NAME')
 
 db_user = os.getenv('DATABASE_USER')
@@ -14,39 +11,41 @@ db_password = os.getenv('DATABASE_PASSWORD')
 db_host = os.getenv('DATABASE_HOST')
 
 
-class DbConnection():
-    """Initializes connection to the database and executes queries"""
-    def __init__(self, configuration=None):
+class DB_conns():
+    """Initialize DB connection"""
+    def __init__(self):
         db_name = os.getenv('DATABASE_NAME')
         db_user = os.getenv('DATABASE_USER')
         db_password = os.getenv('DATABASE_PASSWORD')
         db_host = os.getenv('DATABASE_HOST')
 
+        # connection to PostgreSQL database
         self.conn = psycopg2.connect(
             f"dbname={db_name} user={db_user} password={db_password} host={db_host}")
         self.conn.autocommit = True
         self.cur = self.conn.cursor()
 
+    # query the database
     def query(self, *args):
         self.cur.execute(*args)
 
+    # Commit changes
     def commit(self):
         self.conn.commit()
 
+    # close the connection
     def close(self):
         self.cur.close()
 
 
 def create_app(configuration):
-    """Configures app based on the environment"""
+    """Configure app as specified in the environment in use"""
     app = Flask(__name__, instance_relative_config=True)
     api = Api(app)
     app.config.from_object(config_app[configuration])
+
     from app.resources.entries_resource import AllEntries, SingleEntry
     from app.resources.user_resource import SigninResource, SignupResource
-    # FIXME
-    # use fstring to map url
-    # url = "api/v1" 
 
     api.add_resource(SignupResource, '/api/v1/auth/signup')
     api.add_resource(SigninResource, '/api/v1/auth/signin')
