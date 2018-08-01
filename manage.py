391d6e0d0a34dcb702ee54e25225738a355efc55
Byron Taaka <4dbyron@gmail.com>
2018-08-01 08:50:02 +0300

    Refactored Tables Creation Method

--- manage.py
+++ manage.py
@@ -1,4 +1,16 @@
-from app.models import db_table
 
-db = db_table("db_name = my_diary_db")
-db.create_tables()
+from app import DB_conns
+
+db = DB_conns()
+
+
+def create_tables():
+    db.query(
+        "CREATE TABLE users(id serial PRIMARY KEY, name VARCHAR(100), email VARCHAR(100), username VARCHAR(30), password VARCHAR(100));")
+
+    db.query(
+        "CREATE TABLE entries(entry_id serial, user_id INTEGER REFERENCES users(id), title VARCHAR(200), story TEXT, date_created TIMESTAMP DEFAULT NOW());")
+
+
+if __name__ == '__main__':
+    create_tables()
